2aad60d6f8c34c77b9563962a83e020e6b411b95
MihailTs <mihail.tsvetkov.04@gmail.com>
2026-01-08 14:06:49 +0200

    Initialized project

---
+++ src/test/java/com/mihailTs/trading_bot/TradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihailTs.trading_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
